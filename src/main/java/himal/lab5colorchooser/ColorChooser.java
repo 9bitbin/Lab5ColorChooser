2607ccc998430547f6a2cac5d6ea74f695528206
Himal Shrestha <shresthahimal12@yahoo.com>
2024-10-07 00:22:56 -0400

    i changed the comment and the formatting of the code so it looks neater

--- src/main/java/himal/lab5colorchooser/ColorChooser.java
+++ src/main/java/himal/lab5colorchooser/ColorChooser.java
@@ -1,4 +1,12 @@
 package himal.lab5colorchooser;
+
+/**
+ * Himal Shrestha
+ * R01955141
+ * CSC 325 - Software Engineering
+ * LAB 5 Color Chooser Part 2
+ */
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -6,18 +14,41 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ColorChooser extends Application {
+
+    /**
+     * The start method is the main entry point for JavaFX applications.
+     * It loads the FXML file and sets up the primary stage (window) for the app.
+     *
+     * @param stage the primary stage for this application, onto which
+     *              the application scene can be set.
+     * @throws Exception if the FXML file cannot be loaded.
+     */
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root =
-                FXMLLoader.load(getClass().getResource("ColorChooser.fxml"));
+        // Load the FXML layout for the application from the file "ColorChooser.fxml"
+        Parent root = FXMLLoader.load(getClass().getResource("ColorChooser.fxml"));
 
+        // Create a new Scene with the loaded FXML layout
         Scene scene = new Scene(root);
-        stage.setTitle("Color Chooser");
+
+        // Set the title of the application window
+        stage.setTitle("Himal's Epic Color Chooser");
+
+        // Attach the Scene to the Stage
         stage.setScene(scene);
+
+        // Display the Stage (window) to the user
         stage.show();
     }
 
+    /**
+     * The main method serves as the entry point for the application.
+     * It calls the launch method, which in turn calls the start method.
+     *
+     * @param args the command-line arguments passed to the program.
+     */
     public static void main(String[] args) {
+        // Launch the JavaFX application
         launch(args);
     }
 }
--- src/main/java/himal/lab5colorchooser/ColorChooserController.java
+++ src/main/java/himal/lab5colorchooser/ColorChooserController.java
@@ -1,4 +1,10 @@
 package himal.lab5colorchooser;
+/**
+ * Himal Shrestha
+ * CSC 325 - Software Engineering
+ * LAB 5 Color Chooser Part 2
+ */
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
@@ -6,72 +12,91 @@ import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.converter.NumberStringConverter;
 
 public class ColorChooserController {
-    // instance variables for interacting with GUI components
-    @FXML private Slider redSlider;
-    @FXML private Slider greenSlider;
-    @FXML private Slider blueSlider;
-    @FXML private Slider alphaSlider;
-    @FXML private TextField redTextField;
-    @FXML private TextField greenTextField;
-    @FXML private TextField blueTextField;
-    @FXML private TextField alphaTextField;
-    @FXML private Rectangle colorRectangle;
+    // GUI components for sliders and text fields
+    @FXML
+    private Slider redSlider;
+    @FXML
+    private Slider greenSlider;
+    @FXML
+    private Slider blueSlider;
+    @FXML
+    private Slider alphaSlider;
+    @FXML
+    private TextField redTextField;
+    @FXML
+    private TextField greenTextField;
+    @FXML
+    private TextField blueTextField;
+    @FXML
+    private TextField alphaTextField;
+    @FXML
+    private Rectangle colorRectangle;
 
-    // instance variables for managing
+    // Variables for color values
     private int red = 0;
     private int green = 0;
     private int blue = 0;
     private double alpha = 1.0;
 
+    /**
+     * The initialize method is called after the FXML file is loaded.
+     * It sets up bidirectional bindings between each Slider and its respective TextField,
+     * and adds listeners to update the Rectangle color based on the slider values.
+     */
     public void initialize() {
-        // bind TextField values to corresponding Slider values
-        redTextField.textProperty().bind(
-                redSlider.valueProperty().asString("%.0f"));
-        greenTextField.textProperty().bind(
-                greenSlider.valueProperty().asString("%.0f"));
-        blueTextField.textProperty().bind(
-                blueSlider.valueProperty().asString("%.0f"));
-        alphaTextField.textProperty().bind(
-                alphaSlider.valueProperty().asString("%.2f"));
+        // Bidirectional bindings between TextField and Slider for red color value
+        redTextField.textProperty().bindBidirectional(redSlider.valueProperty(), new NumberStringConverter());
 
-        // listeners that set Rectangle's fill based on Slider changes
+        // Bidirectional bindings between TextField and Slider for green color value
+        greenTextField.textProperty().bindBidirectional(greenSlider.valueProperty(), new NumberStringConverter());
+
+        // Bidirectional bindings between TextField and Slider for blue color value
+        blueTextField.textProperty().bindBidirectional(blueSlider.valueProperty(), new NumberStringConverter());
+
+        // Bidirectional bindings between TextField and Slider for alpha (transparency) value
+        alphaTextField.textProperty().bindBidirectional(alphaSlider.valueProperty(), new NumberStringConverter());
+
+        // Listener for red slider - updates red color component and fills the rectangle
         redSlider.valueProperty().addListener(
                 new ChangeListener<Number>() {
                     @Override
-                    public void changed(ObservableValue<? extends Number> ov,
-                                        Number oldValue, Number newValue) {
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
                         red = newValue.intValue();
                         colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
                     }
                 }
         );
+
+        // Listener for green slider - updates green color component and fills the rectangle
         greenSlider.valueProperty().addListener(
                 new ChangeListener<Number>() {
                     @Override
-                    public void changed(ObservableValue<? extends Number> ov,
-                                        Number oldValue, Number newValue) {
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
                         green = newValue.intValue();
                         colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
                     }
                 }
         );
+
+        // Listener for blue slider - updates blue color component and fills the rectangle
         blueSlider.valueProperty().addListener(
                 new ChangeListener<Number>() {
                     @Override
-                    public void changed(ObservableValue<? extends Number> ov,
-                                        Number oldValue, Number newValue) {
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
                         blue = newValue.intValue();
                         colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
                     }
                 }
         );
+
+        // Listener for alpha slider - updates alpha (transparency) component and fills the rectangle
         alphaSlider.valueProperty().addListener(
                 new ChangeListener<Number>() {
                     @Override
-                    public void changed(ObservableValue<? extends Number> ov,
-                                        Number oldValue, Number newValue) {
+                    public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
                         alpha = newValue.doubleValue();
                         colorRectangle.setFill(Color.rgb(red, green, blue, alpha));
                     }
@@ -79,4 +104,3 @@ public class ColorChooserController {
         );
     }
 }
-
